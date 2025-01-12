0b83f10793526000a78f21e157eb7a83f089d3f5
Unnui <tob.carrea@gmail.com>
2025-01-12 04:35:42 -0300

    Cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,5 @@
 use std::io;
-use gtk4 as gtk;
 
 fn main() {
-
-    println!("Welcome!\nPlease input a username:");
-    let mut user = String::from("user"); 
-    io::stdin()
-            .read_line(&mut user)
-            .expect("Could not read input.");
-    
+    println!("Hi there, good-looking.");    
 }
